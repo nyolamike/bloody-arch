0fe773b9db91550b0a66dc3fe19d60d38b386c84
Nyola Mike a.k.a gr33ndot <iamnyola@gmail.com>
2022-02-18 13:45:50 +0300

    creates a new mix project

---
+++ tag_and_follow/lib/tag_and_follow.ex
@@ -0,0 +1,18 @@
+defmodule TagAndFollow do
+  @moduledoc """
+  Documentation for `TagAndFollow`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TagAndFollow.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ tag_and_follow/test/tag_and_follow_test.exs
@@ -0,0 +1,8 @@
+defmodule TagAndFollowTest do
+  use ExUnit.Case
+  doctest TagAndFollow
+
+  test "greets the world" do
+    assert TagAndFollow.hello() == :world
+  end
+end
